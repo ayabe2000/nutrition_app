63bdbebce4e43bd649737de1ad27484f899cf450
ayabe <rina@ngen.co.jp>
2023-09-29 09:29:24 +0900

    ci test

--- migrations/env.py
+++ migrations/env.py
@@ -7,8 +7,8 @@ import logging
 from logging.config import fileConfig
 
 from flask import current_app
-
-from alembic import context 
+# pylint: disable=no-member
+from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
